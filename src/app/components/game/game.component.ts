05f35f8e2b7687c161075b83b10392d2d02b3758
shahrzad <shahrzadjkoushesh@gmail.com>
2023-08-07 09:21:22 +0330

    hover squares

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+
+  board!: Array<string>;
+
+  constructor(){
+    // create an array with 9 empty elements
+    this.board = new Array(9).fill('');
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  handlePlayer(index: number){
+    this.board[index] = (index+1).toString();
+  }
 
 }
