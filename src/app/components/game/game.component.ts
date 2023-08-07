2c535479469d8234cd0f0959fd345137eafb1561
shahrzad <shahrzadjkoushesh@gmail.com>
2023-08-07 10:43:34 +0330

    add game logic

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
 
   board!: Array<string>;
+  winningBoard = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  playerTurn = 'x';
 
+  selectedByX : number[] = [];
+  selectedByO : number[] = [];
+  
   constructor(){
     // create an array with 9 empty elements
     this.board = new Array(9).fill('');
@@ -20,7 +25,33 @@ export class GameComponent implements OnInit {
   }
 
   handlePlayer(index: number){
-    this.board[index] = (index+1).toString();
+    this.board[index] = this.playerTurn;
+    if (this.playerTurn == 'x'){
+      this.selectedByX.push(+this.board[index])
+      this.playerTurn = 'o'
+    }
+    else{
+      this.selectedByO.push(+this.board[index])
+      this.playerTurn = 'x'
+    }
+    
+    for(let i = 0; i < this.winningBoard.length; i++){
+      let winningCheck = this.winningBoard[i];
+      let p1 = winningCheck[0];
+      let p2 = winningCheck[1];
+      let p3 = winningCheck[2];
+      if (this.board[p1] != '' && this.board[p1] == this.board[p2] && this.board[p3] == this.board[p2] && this.board[p1] == this.board[p3]){
+        if (this.playerTurn == 'x'){
+          this.playerTurn = 'o'
+        }
+        else{
+          this.playerTurn = 'x'
+        }
+        alert("Player " +this.playerTurn+ " Won")
+        // console.log("GHJK")
+      }
+    }
+
   }
 
 }
